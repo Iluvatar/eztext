434b2463c1b102e99360e49832b0937a2c297281
Tyler Yeats <jsjoesmith6@gmail.com>
2015-10-29 09:31:26 -0700

    Created better examples

--- example.py
+++ example.py
@@ -1,45 +1,50 @@
 # EzText example
 from pygame.locals import *
-import pygame, sys, eztext
+import pygame
+
+import eztext
+
 
 def main():
-    # initialize pygame
     pygame.init()
-    # create the screen
-    screen = pygame.display.set_mode((640,240), pygame.RESIZABLE)
-    # fill the screen w/ white
-    screen.fill((255,255,255))
-    # here is the magic: making the text input
-    # create an input with a max length of 45,
-    # and a red color and a prompt saying 'type here: '
-    txtbx1 = eztext.Input(max_length=30, prompt='', x=10, y=10, font=pygame.font.SysFont('monospace', 24), input_width=420, default_text="First Name")
-    txtbx2 = eztext.Input(max_length=10, prompt='', x=10, y=44, font=pygame.font.SysFont('monospace', 24), input_width=140, default_text="Last Name")
+
+    screen = pygame.display.set_mode((640, 240), pygame.RESIZABLE)
+    screen.fill((255, 255, 255))
+
+    # create an input with purple text at (100, 100) that takes at most 7 characters
+    txtbx1 = eztext.Input(pos=(100, 100), text_color=(180, 20, 180), max_length=7)
+
+    # create an input that only takes numbers
+    txtbx2 = eztext.Input(input_width=150, allowed_chars=eztext.Input.NUMS, default_text="numbers only",
+                          set_key_repeat_speed=True)
+
     # create the pygame clock
     clock = pygame.time.Clock()
-    # main loop!
 
     while 1:
-        # make sure the program is running at 30 fps
         clock.tick(30)
 
-        # events for txtbx
+        # events for the inputs
         events = pygame.event.get()
+
         # process other events
         for event in events:
-            # close it x button si pressed
-            if event.type == QUIT: return
+            if event.type == QUIT:
+                return
             if event.type == pygame.VIDEORESIZE:
-                txtbx2.set_width(event.size[0] / 2)
+                txtbx2.input_width = event.size[0] / 2
                 screen = pygame.display.set_mode(event.size, pygame.RESIZABLE)
 
-        # clear the screen
-        screen.fill((255,255,255))
-        # update txtbx
+        screen.fill((255, 255, 255))
+
+        # update inputs
         txtbx1.update(events)
         txtbx1.draw(screen)
         txtbx2.update(events)
         txtbx2.draw(screen)
-        # refresh the display
+
         pygame.display.flip()
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
