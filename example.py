89560d4b6042509f29c9005b87201822234c0b14
Tyler Yeats <jsjoesmith6@gmail.com>
2015-11-30 22:03:48 -0800

    Renamed to pyform to reflect added functionality.

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
-# EzText example
+# pyform example
 from pygame.locals import *
 import pygame
 
-import eztext
+import pyform
 
 
 def main():
     pygame.init()
 
-    screen = pygame.display.set_mode((640, 240), pygame.RESIZABLE)
+    screen = pygame.display.set_mode((640, 360), pygame.RESIZABLE)
     screen.fill((255, 255, 255))
 
     # returns a function that prints the value of the given form
@@ -20,28 +20,29 @@ def main():
         return ret
 
     # checkbox group with 4 options
-    checkgroup = eztext.CheckBoxGroup("checkgroup")
-    checkgroup.add_checkbox(eztext.CheckBox("test1", "Test Pod 14", pos=(10, 130), text_color=(120, 20, 120)))
-    checkgroup.add_checkbox(eztext.CheckBox("test2", "Test Pod 15", pos=(10, 170), text_color=(120, 20, 120)))
-    checkgroup.add_checkbox(eztext.CheckBox("test3", "Test Pod 16", pos=(10, 210), text_color=(120, 20, 120)))
-    checkgroup.add_checkbox(eztext.CheckBox("test4", "Test Pod 17", pos=(10, 250), text_color=(120, 20, 120)))
+    checkgroup = pyform.CheckBoxGroup("checkgroup")
+    checkgroup.add_checkbox(pyform.CheckBox("test1", "Test Pod 14", pos=(10, 130), text_color=(120, 20, 120)))
+    checkgroup.add_checkbox(pyform.CheckBox("test2", "Test Pod 15", pos=(10, 170), text_color=(120, 20, 120)))
+    checkgroup.add_checkbox(pyform.CheckBox("test3", "Test Pod 16", pos=(10, 210), text_color=(120, 20, 120)))
+    checkgroup.add_checkbox(pyform.CheckBox("test4", "Test Pod 17", pos=(10, 250), text_color=(120, 20, 120)))
 
     # radio button group with 4 options
-    radiogroup = eztext.RadioGroup("radiogroup")
-    radiogroup.add_button(eztext.RadioButton("c1", "Choice 1", pos=(400, 10), text_color=(120, 20, 120)))
-    radiogroup.add_button(eztext.RadioButton("c2", "Choice 2", pos=(400, 50), text_color=(120, 20, 120)))
-    radiogroup.add_button(eztext.RadioButton("c3", "Choice 3", pos=(400, 90), text_color=(120, 20, 120)))
-    radiogroup.add_button(eztext.RadioButton("c4", "Choice 4", pos=(400, 130), text_color=(120, 20, 120)))
+    radiogroup = pyform.RadioGroup("radiogroup")
+    radiogroup.add_button(pyform.RadioButton("c1", "Choice 1", pos=(400, 10), text_color=(120, 20, 120)))
+    radiogroup.add_button(pyform.RadioButton("c2", "Choice 2", pos=(400, 50), text_color=(120, 20, 120)))
+    radiogroup.add_button(pyform.RadioButton("c3", "Choice 3", pos=(400, 90), text_color=(120, 20, 120)))
+    radiogroup.add_button(pyform.RadioButton("c4", "Choice 4", pos=(400, 130), text_color=(120, 20, 120)))
 
     # form object for holding all the elements
-    form = eztext.Form("main")
+    form = pyform.Form("main")
 
     # adding text boxes to the form directly
-    form.add_form_object(eztext.TextInput("txt1", pos=(10, 10), prompt="First name ", input_width=240, max_length=16,
-                                          set_key_repeat_speed=True))
-    form.add_form_object(eztext.TextInput("txt2", pos=(10, 50), prompt="Last name ", input_width=240, max_length=16))
-    form.add_form_object(eztext.TextInput("txt3", pos=(10, 90), prompt="Age ", input_width=50, max_length=4,
-                                          allowed_chars=eztext.TextInput.NUMS))
+    form.add_form_object(pyform.TextInput("txt1", pos=(10, 10), default_text="First name", input_width=360,
+                                          max_length=16, set_key_repeat_speed=True))
+    form.add_form_object(pyform.TextInput("txt2", pos=(10, 50), default_text="Last name", input_width=360,
+                                          max_length=16))
+    form.add_form_object(pyform.TextInput("txt3", pos=(10, 90), default_text="Age", input_width=360, max_length=4,
+                                          allowed_chars=pyform.TextInput.NUMS))
 
     # add checkbox and radio groups
     form.add_form_object(checkgroup)
